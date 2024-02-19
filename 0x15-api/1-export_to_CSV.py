2f9f5d4eca62f0e465ed662270d02b6ab4e151b0
Salah Eddine <shen.real@live.fr>
2024-02-19 02:18:33 -1200

    done task

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Using what you did in the task #0,
+extend your Python script to export data in the CSV format.
+"""
+import requests
+from sys import argv
+import csv
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/user"
+    name = requests.get(url + "s/{}".format(argv[1])).json().get("username")
+    todos = requests.get(url + "/{}/todos".format(argv[1])).json()
+
+    with open("{}.csv".format(argv[1]), mode="w") as f:
+        csv_writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for todo in todos:
+            rows = [argv[1], name, todo.get("completed"), todo.get("title")]
+            csv_writer.writerow(rows)
